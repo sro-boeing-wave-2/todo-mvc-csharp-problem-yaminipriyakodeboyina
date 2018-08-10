a49e64b587b61828a35d92a1f965591f8255af29
yamini priya <yaminipriyakodeboyina@gmail.com>
2018-08-10 18:43:44 +0530

    added docker

---
+++ finaltodo/Controllers/ValuesController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace finaltodo.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        [HttpGet]
+        public List<String> GetValues()
+        {
+            return new List<string> { "Value1", "Value2" };
+        }
+    }
+}
